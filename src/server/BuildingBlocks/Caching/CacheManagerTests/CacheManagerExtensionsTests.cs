a3a6fe53c33151ea2287624011f7752091858c0d
Saturn Technologies <info@saturn72.com>
2018-05-14 12:27:51 +0300

    restructure solution (create subdirectories for Caching and integration)

---
+++ src/server/BuildingBlocks/Caching/CacheManagerTests/CacheManagerExtensionsTests.cs
@@ -0,0 +1,79 @@
+using System;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace CacheManager.Tests
+{
+    public class CacheManagerExtensionsTests
+    {
+        [Fact]
+        public void CacheManagerExtensions_Set()
+        {
+            var cm = new Mock<ICacheManager>();
+            var key = "key";
+            var value = new CacheTestObject();
+            CacheManagerExtensions.Set(cm.Object, key, value);
+            cm.Verify(c => c.Set(
+                It.Is<string>(k => k == key),
+                It.Is<CacheTestObject>(cto => cto == value),
+                It.Is<uint>(i => i == CacheManagerExtensions.DefaultCacheTime)), Times.Once);
+        }
+
+        [Fact]
+        public void CacheManagerExtensions_SetIfNotExists()
+        {
+            var cm = new Mock<ICacheManager>();
+
+            var key = "key";
+            var value = new CacheTestObject();
+            var cacheTime = 123U;
+
+            //object is cached
+            cm.Setup(c => c.Get<CacheTestObject>(It.IsAny<string>())).Returns(null as CacheTestObject);
+
+            CacheManagerExtensions.SetIfNotExists(cm.Object, key, value, cacheTime);
+            cm.Verify(c => c.Set(
+                    It.Is<string>(k => k == key),
+                    It.Is<CacheTestObject>(cto => cto == value),
+                    It.IsAny<uint>()),
+                Times.Once);
+
+            //object is not cached
+            cm.Reset();
+            cm.Setup(c => c.Get<CacheTestObject>(It.IsAny<string>())).Returns(new CacheTestObject());
+            CacheManagerExtensions.SetIfNotExists(cm.Object, key, value, cacheTime);
+            cm.Verify(c => c.Set(It.IsAny<string>(), It.IsAny<CacheTestObject>(), It.IsAny<uint>()),
+                Times.Never);
+        }
+
+        [Fact]
+        public void CacheManagerExtensions_GetOrAquire()
+        {
+            var cm = new Mock<ICacheManager>();
+            var key = "key";
+            var cacheTime = 123U;
+            var wasAquired = false;
+            Func<CacheTestObject> aquireFunc = () =>
+            {
+                wasAquired = true;
+                return new CacheTestObject();
+            };
+            //does not call aquire func
+            cm.Setup(c => c.Get<CacheTestObject>(It.IsAny<string>())).Returns(new CacheTestObject());
+
+            CacheManagerExtensions.Get(cm.Object, key, aquireFunc, cacheTime);
+            wasAquired.ShouldBeFalse();
+
+            //should call call aquire func
+            cm.Setup(c => c.Get<CacheTestObject>(It.IsAny<string>())).Returns(null as CacheTestObject);
+            CacheManagerExtensions.Get(cm.Object, key, aquireFunc, cacheTime);
+            wasAquired.ShouldBeTrue();
+        }
+
+        internal class CacheTestObject
+        {
+
+        }
+    }
+}
